be31dea1c494d7cc1e37950c27bfb2a73bd67312
kirill <kirill.prokofiev@intel.com>
2020-08-06 17:23:50 +0300

    reader for LCFAD test

---
+++ reader_dataset_tmp.py
@@ -0,0 +1,37 @@
+import os
+import torch
+from torch.utils.data import Dataset
+import cv2 as cv
+import albumentations as A
+import numpy as np
+
+
+class LCFAD_test(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        name_of_real_img = filter(lambda x: x.startswith('real'), 
+                    os.listdir(os.path.join(root_dir, 'LCC_FASD_evaluation')))
+        real_img = list(map(lambda x: os.path.join('LCC_FASD_evaluation', x), name_of_real_img))
+
+        name_of_spoof_img = filter(lambda x: x.startswith('spoof'), 
+                    os.listdir(os.path.join(root_dir, 'LCC_FASD_evaluation')))
+        spoof_img = list(map(lambda x: os.path.join('LCC_FASD_evaluation', x), name_of_spoof_img))
+            
+        self.list_img = spoof_img + real_img
+        self.labels = torch.cat((torch.ones(len(spoof_img), dtype=torch.long),
+                                    (torch.zeros(len(real_img), dtype=torch.long))))
+
+    def __len__(self):
+        return self.labels.size(0)
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.list_img[index])
+        image = cv.imread(img_path, flags=1)
+        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        if self.transform:
+            image = self.transform(image=image)['image']
+        # [batch, channels, height, width]
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        y_label = self.labels[index]
+        return (torch.tensor(image), y_label)
