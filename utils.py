2456a6c3ab972a6809fd0cea83e7d575b1100b5c
kirill <kirill.prokofiev@intel.com>
2020-08-28 11:00:32 +0300

    fix model for cross entropy

--- utils.py
+++ utils.py
@@ -13,7 +13,40 @@ from losses import AngleSimpleLinear, SoftTripleLinear, AMSoftmaxLoss, SoftTripl
 import torch.nn as nn
 from models import mobilenetv2, mobilenetv3_large, mobilenetv3_small
 import json
-from models import Dropout
+
+
+class Dropout(nn.Module):
+    DISTRIBUTIONS = ['bernoulli', 'gaussian', 'none']
+
+    def __init__(self, p=0.5, mu=0.5, sigma=0.2, dist='bernoulli'):
+        super(Dropout, self).__init__()
+
+        self.dist = dist
+        assert self.dist in Dropout.DISTRIBUTIONS
+
+        self.p = float(p)
+        assert 0. <= self.p <= 1.
+
+        self.mu = float(mu)
+        self.sigma = float(sigma)
+        assert self.sigma > 0.
+
+    def forward(self, x):
+        if self.dist == 'bernoulli':
+            out = F.dropout(x, self.p, self.training)
+        elif self.dist == 'gaussian':
+            if self.training:
+                with torch.no_grad():
+                    soft_mask = x.new_empty(x.size()).normal_(self.mu, self.sigma).clamp_(0., 1.)
+
+                scale = 1. / self.mu
+                out = scale * soft_mask * x
+            else:
+                out = x
+        else:
+            out = x
+
+        return out
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
@@ -274,7 +307,7 @@ def build_model(config, args, strict=True):
             model.classifier[1] == Dropout(dist=config['dropout']['type'], mu=config['dropout']['mu'], 
                                                         sigma=config['dropout']['sigma'], 
                                                         p=config['dropout']['classifier'])
-
+            model.classifier[2] = nn.BatchNorm1d(config['model']['embeding_dim'])
             model.classifier[4] = nn.Linear(1280, 2)
 
         else:
